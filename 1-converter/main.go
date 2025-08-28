93b82b226e973041e3d2178fd3ad1da59049bfe1
YSDev <a.yandukin@gmail.com>
2025-08-29 00:52:20 +0300

    dz4

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 
 }
@@ -7,3 +9,20 @@ func main() {
 const usdEurRate = 1.3
 const usdRubRate = 89.5
 const eurRubRate = usdEurRate * usdRubRate
+
+func getUserInput() (amount float64, from string, to string) {
+	fmt.Print("Введите сумму: ")
+	fmt.Scan(&amount)
+
+	fmt.Print("Введите исходную валюту: ")
+	fmt.Scan(&from)
+
+	fmt.Print("Введите целевую валюту: ")
+	fmt.Scan(&to)
+
+	return
+}
+
+func calculateRate(amount float64, from string, to string) float64 {
+
+}
