ad6f069c3eea8821ee4c7038b078a9dd3c76218e
ysdevgp <dayly_21rus@hotmail.com>
2025-09-29 01:43:06 +0300

    dz7

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -14,9 +14,10 @@ func main() {
 
 var currencies = []string{"USD", "EUR", "RUB"}
 
-var usdRates = map[string]float64{
-	"EUR": 1.3,
-	"RUB": 89.5,
+var rates = map[string]map[string]float64{
+	"EUR": {"USD": 1.3, "RUB": 116.35},
+	"USD": {"EUR": 0.76, "RUB": 89.5},
+	"RUB": {"EUR": 1.3, "USD": 0.011},
 }
 
 func getUserInput() (amount float64, from string, to string) {
@@ -57,22 +58,5 @@ func getAmount(text string) float64 {
 }
 
 func calculateRate(amount float64, from string, to string) float64 {
-	var result float64
-
-	switch {
-	case from == "USD" && to == "EUR":
-		result = amount * usdRates["EUR"]
-	case from == "EUR" && to == "USD":
-		result = amount / usdRates["EUR"]
-	case from == "USD" && to == "RUB":
-		result = amount * usdRates["RUB"]
-	case from == "RUB" && to == "USD":
-		result = amount / usdRates["RUB"]
-	case from == "EUR" && to == "RUB":
-		result = amount * usdRates["EUR"] * usdRates["RUB"]
-	case from == "RUB" && to == "EUR":
-		result = amount / (usdRates["EUR"] * usdRates["RUB"])
-	}
-
-	return result
+	return amount * rates[from][to]
 }
